2b72a89cf718cee3d827639643a8a457c0c98ee3
Devyanezeo <eliseo.yanez@usach.cl>
2025-05-14 20:45:52 -0400

    kart service

---
+++ kart-service/src/main/java/com/tingeso/kart_service/repository/KartRepository.java
@@ -0,0 +1,4 @@
+package com.tingeso.kart_service.repository;
+
+public interface KartRepository {
+}
---
+++ kart-service/src/test/java/com/tingeso/kart_service/KartServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tingeso.kart_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KartServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

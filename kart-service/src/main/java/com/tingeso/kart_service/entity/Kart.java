1c60205ac3a8a671a3549d950d3786dede8c4fbe
DevYanezeo <eliseo.yanez@usach.cl>
2025-05-20 12:29:23 -0400

    Fix: Pom.xml

--- kart-service/src/main/java/com/tingeso/kart_service/entity/Kart.java
+++ kart-service/src/main/java/com/tingeso/kart_service/entity/Kart.java
@@ -1,32 +1,23 @@
 package com.tingeso.kart_service.entity;
 
-import jakarta.persistence.*;
 import lombok.*;
 
 import java.time.LocalDate;
-import java.util.ArrayList;
-import java.util.List;
 
-@Entity
+
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-@Table(name = "Kart")
 public class Kart {
-    @Id
-    @Column(name = "kart_code")
+
     private String kartCode;
 
-    @Column(nullable = false)
     private String model;
 
-    @Column(nullable = false)
     private boolean underMaintenance = false;
 
     private String description;
 
-    // Relación con reservas
-    @Column(name = "last_maintenance")
     private LocalDate lastMaintenance;
 
 }
